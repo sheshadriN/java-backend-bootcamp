d09c005a483f5128f4bbd16c3b74692e3a483898
Vijaysai <vijaysai@example.com>
2023-03-05 14:26:05 +0530

    c

--- session-03-java-basics/java-basics-exercises/src/main/java/io/javabrains/javabasics/AnonymousInnerClasses.java
+++ session-03-java-basics/java-basics-exercises/src/main/java/io/javabrains/javabasics/AnonymousInnerClasses.java
@@ -7,6 +7,6 @@ Create an anonymous inner class implementation of `Animal2` and then call the `m
 
 public class AnonymousInnerClasses {
     public static void main(String[] args) {
-
+sywuitsiute
     }
 }
